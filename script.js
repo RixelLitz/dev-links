633dc7170213e597c2f2a5268e584ba2fdaae157
Rian Britto Pereira <contatorianbritto@gmail.com>
2024-01-27 00:29:50 -0300

    Atualizações

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleMode() {
   html.classList.toggle("light") // fazendo a troca de temas
   const img = document.querySelector("#profile img") //pegar o seletor da imagem
   if (html.classList.contains("light")) { //se tiver light mode adicionar imagem light
-    img.setAttribute("src", "assets/avatar-light.png")
+    img.setAttribute("src", "assets/rian-avatar-light.png")
   } else { // se nao tiver em modo light, manter a foto original
     img.setAttribute("src", "assets/avatar.png")
   }
